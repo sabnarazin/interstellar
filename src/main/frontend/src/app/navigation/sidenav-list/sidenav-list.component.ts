1c51a37a1f3af8982948f058c80b71f9e2490dc7
sabnarazin <76424869+sabnarazin@users.noreply.github.com>
2021-08-10 00:00:01 +0200

    Update sidenav-list.component.ts

--- src/main/frontend/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/main/frontend/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {Router} from '@angular/router';
 import { UserService } from 'src/app/service/user.service';
- 
+
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
@@ -9,12 +9,12 @@ import { UserService } from 'src/app/service/user.service';
 })
 export class SidenavListComponent implements OnInit {
   @Output() sidenavClose = new EventEmitter();
- 
+
   constructor(private router: Router, private auth: UserService) { }
- 
+
   ngOnInit() {
   }
- 
+
   public logout = () => {
     this.sidenavClose.emit();
     console.log("Navigated to Logout");
@@ -28,5 +28,27 @@ export class SidenavListComponent implements OnInit {
     //});
     //this.router.navigate(['../../metric']);
   }
- 
+  click1() : void {
+    this.router.navigate(['dashboard']);
+  }
+  click2() : void {
+    this.router.navigate(['addplanet']);
+  }
+  click3() : void {
+    this.router.navigate(['addroute']);
+  }
+  click4() : void {
+    this.router.navigate(['addtraffic']);
+  }
+  click5() : void {
+    this.router.navigate(['vieweditplanet']);
+  }
+  click6() : void {
+    this.router.navigate(['vieweditroute']);
+  }
+  click7() : void {
+    this.router.navigate(['viewedittraffic']);
+  }
+
+
 }
